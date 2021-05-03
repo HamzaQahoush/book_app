e1312947de7b76f356dc6fda68b8d7061b0497c2
HamzaQahoush <hamza.qah@gmail.com>
2021-05-03 23:25:04 +0300

    try1

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 // Import packages
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
+
 const superagent = require('superagent');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.get('/hello', (req, res) => {
-  res.render('pages/index');
-});
 
-app.get('/searches/new', (req, res) => {
-  res.render('pages/searches/new.ejs');
-});
-app.get('/', (req, res) => {
-  res.render('pages/index');
-});
-app.post('/searches', searchHandler);
+app.get('/new', formSearch);
+app.get('/', homePage);
+app.post('/searches', searchResult);
+app.get('/books/:bookID', bookDetails);
+app.post('/books', addBook);
+
+function formSearch(req, res) {
+  res.render('pages/searches/new');
+}
 
 //search function
-function searchHandler(req, res) {
+function searchResult(req, res) {
   let url;
   let input = req.body.search;
   if (req.body.searchBy === 'title') {
@@ -37,16 +41,58 @@ function searchHandler(req, res) {
     .get(url)
     .then(bookApiData => bookApiData.body.items.map(item => new Book(item)))
     .then(element => {
+      console.log(element);
       res.render('pages/searches/show', { books: element });
     })
     .catch(err => console.log(err));
 }
 
 function Book(data) {
-  this.title = data.volumeInfo.title;
-  this.authors = data.volumeInfo.authors;
-  this.description = data.volumeInfo.description;
-  this.image = data.volumeInfo.imageLinks.smallThumbnail;
+  this.title = data.volumeInfo.title || 'not available';
+  this.authors =
+    data.volumeInfo.author !== undefined
+      ? data.volumeInfo.authors
+      : 'not available';
+  this.description = data.volumeInfo.description || 'not available';
+  this.image = data.volumeInfo.imageLinks.smallThumbnail || 'not available';
+}
+
+function homePage(req, res) {
+  let SQL = `SELECT * FROM booklist;`;
+  client
+    .query(SQL)
+    .then(allbooks => {
+      console.log(allbooks);
+      res.render('pages/index', { booklist: allbooks.rows });
+    })
+    .catch(err => console.log(err));
+}
+
+function bookDetails(req, res) {
+  let SQL = `SELECT * from books WHERE id=${req.params.bookID};`;
+  client.query(SQL).then(result => {
+    console.log(result);
+    res.render('pages/books/show', { book: result.rows[0] });
+  });
+}
+
+function addBook(req, res) {
+  let SQL = `INSERT INTO booklist (title, authors, description, img, isbn, shelf) VALUES($1,$2,$3,$4,$5,$6) RETURNING id;`;
+  let SafeValues = [
+    req.body.title,
+    req.body.authors,
+    req.body.description,
+    req.body.img,
+    req.body.isbn,
+    req.body.shelf,
+  ];
+  client
+    .query(SQL, SafeValues)
+    .then(result => {
+      console.log(result, 'kkkkkkkkkkkkkkkkkk`');
+      res.redirect(`/books/${result.rows[0].id}`); //anas
+    })
+    .catch(err => console.log(err));
 }
 
 app.listen(PORT, () => {
